49f72fd72737b42347676e4975ebe119a2a66e5b
aurelia <aureliayarcia@gmail.com>
2025-12-04 22:23:53 +0800

    for lan na, tapos paayos ng employee side

--- app.py
+++ app.py
@@ -1249,4 +1249,5 @@ def update_treatment_type_distribution():
     treatment_type_distribution = treatment_type_data
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
